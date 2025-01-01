5e05c9df9b784c0b09abb48b2e88ab6828507d7a
Mateusz Woźniak <mateusz.wozniak.0211@gmail.com>
2025-01-01 11:54:33 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import flask
+from dotenv import load_dotenv
+import os
+from pymongo import MongoClient
+
+app = flask.Flask(__name__)
+load_dotenv()
+CLIENT = MongoClient(os.getenv('MONGO_URI'))
+DB = CLIENT['sets']
+SETS_COLLECTION = DB['sets']
+
+@app.route('/')
+def index():
+    return str(SETS_COLLECTION.count_documents({}))
+
+@app.route('/sets/<id>')
+def get_set(id):
+    result = list(SETS_COLLECTION.find({"_id": id}))
+    return flask.jsonify(result)
+
+if __name__ == '__main__':
+    print(CLIENT.server_info())
+    app.run(debug=True)
