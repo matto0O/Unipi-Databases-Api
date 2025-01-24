0fb4fe5199921c3d6441caf0bdcfeda344eaeb16
Alicja Bargiela <ala.bargiela@gmail.com>
2025-01-24 21:49:42 +0100

    main update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from user import users_api
 from parts import parts_api
 from colors import colors_api
 from sets import sets_api
+from stats import stats_api
 from imports import *
 
 app = Flask(__name__)
@@ -9,6 +10,7 @@ app.register_blueprint(users_api, url_prefix='/users')
 app.register_blueprint(parts_api, url_prefix='/parts')
 app.register_blueprint(colors_api, url_prefix='/colors')
 app.register_blueprint(sets_api, url_prefix='/sets')
+app.register_blueprint(stats_api, url_prefix='/stats')
 SETS_COLLECTION = DB['sets']
 
 @app.route('/')
