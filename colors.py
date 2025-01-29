4f6e9fa38172f7fea7f27e45563fe1c236d136bf
Mateusz Woźniak <mateusz.wozniak.0211@gmail.com>
2025-01-29 23:38:13 +0100

    colors moved to redis

--- colors.py
+++ colors.py
@@ -1,29 +1,23 @@
 from imports import *
 
 colors_api = Blueprint('colors_api', __name__)
-# COLORS_COLLECTION = DB['colors']
+COLORS_COLLECTION = DB['colors']
 
 @colors_api.route('')
+@redis_cache(module='colors')
 def get_colors():
-    keys = REDIS.keys("colors:*")
-    results = {}
-    for key in keys:
-        _, id, attr = key.split(":")
-        if id not in results:
-            results[id] = {}
-        if attr == "name":
-            results[id]['name'] = REDIS.get(key)
-        else:
-            results[id]['rgb'] = REDIS.get(key)
-    return jsonify(results)
+    result = list(COLORS_COLLECTION.find())
+    for color in result:
+        color['_id'] = str(color['_id'])
+    return jsonify(result) 
 
 @colors_api.route('/<id>')
+@redis_cache(module='colors')
 def get_color(id):
-    name = REDIS.get(f"colors:{id}:name")
-    rgb = REDIS.get(f"colors:{id}:rgb")
-    if not name or not rgb:
+    result = list(COLORS_COLLECTION.find({"_id": int(id)}))
+    if not result:
         return jsonify({'error': 'Color not found'}), 404
-    return jsonify({'_id': id, 'name': name, 'rgb': rgb}) 
+    return jsonify(result) 
 
 #  add new color (include id in request)
 @colors_api.route('', methods=['POST'])
@@ -40,28 +34,52 @@ def add_color():
 
         new_color['_id'] = int(new_color['_id'])
 
-        name = REDIS.get(f"colors:{new_color['_id']}:name")
-        rgb = REDIS.get(f"colors:{new_color['_id']}:rgb")
-        if name or rgb:
+        existing_color = COLORS_COLLECTION.find_one({'_id': new_color['_id']})
+        if existing_color:
             return jsonify({'error': f'Color with _id {new_color["_id"]} already exists'}), 409
 
-        REDIS.set(f"colors:{new_color['_id']}:name", new_color['name'])
-        REDIS.set(f"colors:{new_color['_id']}:rgb", new_color['rgb'])
+        COLORS_COLLECTION.insert_one(new_color)
 
-        return "Color added successfully", 201
+        new_color['_id'] = str(new_color['_id'])
+        response = jsonify(new_color)
+        response.status_code = 201
+        return response
 
     except Exception as e:
 
         return jsonify({'error': f'Internal server error: {str(e)}'}), 500
 
 
+@colors_api.route('/<id>', methods=['PUT'])
+def update_color(id):
+    updated_data = request.get_json()
+
+    if not isinstance(updated_data, dict):
+        return jsonify({'error': 'Input must be a JSON object'}), 400
+
+    try:
+        result = COLORS_COLLECTION.update_one({"_id": int(id)}, {"$set": updated_data})
+
+        if result.matched_count == 0:
+            return jsonify({'error': 'Color not found'}), 404
+
+        updated_color = COLORS_COLLECTION.find_one({"_id": int(id)})
+        updated_color['_id'] = str(updated_color['_id'])
+
+        response = jsonify({'message': 'Color updated successfully', 'color': updated_color})
+        response.status_code = 200
+        return response
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
 @colors_api.route('/<id>', methods=['DELETE'])
 def delete_color(id):
     try:
-        r1 = REDIS.delete(f"colors:{id}:name")
-        r2 = REDIS.delete(f"colors:{id}:rgb")
+        result = COLORS_COLLECTION.delete_one({"_id": int(id)})
 
-        if r1 == 0 and r2 == 0:
+        if result.deleted_count == 0:
             return jsonify({'error': 'Color not found'}), 404
             
         response = jsonify({'message': 'Color deleted successfully'})
