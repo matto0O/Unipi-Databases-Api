1954ab1b43741500a788bba6ce8c7d94c0a42216
Mateusz Woźniak <mateusz.wozniak.0211@gmail.com>
2025-02-15 13:30:36 +0100

    redis cache overcaching bug fix

--- imports.py
+++ imports.py
@@ -23,30 +23,37 @@ def timeit(func):
         return result 
     return wrap 
 
-def redis_cache(module, expire=60): 
+def redis_cache(module, expire=60, limit=None): 
     '''Decorator that saves query result in redis.
     
     Args:
         module (str): The module name.
         expire (int): The expiration time in minutes.
     '''
+
     def decorator(func):
         def wrap(*args, **kwargs): 
             query = f"requests:{module}:{func.__name__}:"
+            if limit:
+                query += str(limit)
             for i, item in enumerate(kwargs):
                 if i != 0:
                     query += '-'
                 query += f"{kwargs[item]}"
+            
+            for i, item in enumerate(args):
+                query += '-'
+                query += f"{item}"
 
             if a:=REDIS.get(query):
                 return jsonify(a)
         
             result = func(*args, **kwargs)
-            if result.status_code != 200:
+            if result[1] != 200:
                 print('Not caching error response...')
                 return result
             print('Inserting into cache...')
-            REDIS.set(query, str(result.json), ex=expire*60)
+            REDIS.set(query, str(result[0].json), ex=expire*60)
             return result
         wrap.__name__ = func.__name__
         return wrap
